55b20599f2e323e9d4d42ac225fbf4bc4d9a8a22
gabrielplima36 <115594832+gabrielplima36@users.noreply.github.com>
2022-11-18 21:37:07 -0300

    estrutura de dados com java

---
+++ array.java
@@ -0,0 +1,43 @@
+package correcaoArquivo;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Quantas notas deseja cadastrar?");
+		int quantNotas = entrada.nextInt();
+		double media = 0;
+		
+		
+		
+		double notas[] = new double[quantNotas];
+		
+		
+		
+		for(int i = 0; i < notas.length; i++) {
+			System.out.println("Digite a "+(i+1)+"° nota: ");
+			
+			notas[i]=entrada.nextDouble();
+			media = media * notas[i];
+			media = media/notas[i];
+			
+		}
+		
+		media = media/notas.length;
+		
+		System.out.print("A média é:"+media);
+		
+		
+		
+		
+		
+		entrada.close();
+		
+
+	}
+
+}
